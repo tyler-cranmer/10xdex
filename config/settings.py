6426ad4267c77d5395799cf235844b7373ce44fe
tyler-cranmer <tyler.cranmer@outlook.com>
2024-05-30 21:45:53 -0600

    adding in more env variables

--- config/settings.py
+++ config/settings.py
@@ -18,7 +18,12 @@ class Settings(BaseSettings):
     POSTGRES_PORT: str = Field(validation_alias="POSTGRES_PORT")
     POSTGRES_HOST: str = Field(validation_alias="POSTGRES_HOST")
     debank_api_key: str = Field(validation_alias="DEBANK_API_KEY")
-
+    base_api_key: str = Field(validation_alias='BASE_API_KEY')
+    base_rpc: str = Field(validation_alias='BASE_HTTPS_URL')
+    base_websocket: str = Field(validation_alias='BASE_WEBSOCKET_URL')
+    account_pk: str = Field(validation_alias='ACCOUNT_1_PK')
+    jina_api_key : str = Field(validation_alias='JINA_API_KEY')
+    coin_gecko_api_key : str = Field(validation_alias='COIN_GECKO_API_KEY')
 
 def get_postgres_uri(settings: Settings = Settings()) -> str:
     return f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:5432/{settings.POSTGRES_DB}"
