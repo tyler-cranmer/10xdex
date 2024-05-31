4750e87c9d3142016915297b19bd5ff370cff14d
tyler-cranmer <tyler.cranmer@outlook.com>
2024-05-31 10:42:30 -0600

    abis

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ class Settings(BaseSettings):
     base_api_key: str = Field(validation_alias='BASE_API_KEY')
     base_rpc: str = Field(validation_alias='BASE_HTTPS_URL')
     base_websocket: str = Field(validation_alias='BASE_WEBSOCKET_URL')
+    base_scan_api_key: str = Field(validation_alias='BASE_SCAN_API_KEY')
     account_pk: str = Field(validation_alias='ACCOUNT_1_PK')
     jina_api_key : str = Field(validation_alias='JINA_API_KEY')
     coin_gecko_api_key : str = Field(validation_alias='COIN_GECKO_API_KEY')
