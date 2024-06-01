3f2b98b27b8858d1f8905ac860a06a8c428d69fb
tyler-cranmer <tyler.cranmer@outlook.com>
2024-05-31 21:51:50 -0600

    retrieved top router abis and contract addresses

--- config/settings.py
+++ config/settings.py
@@ -12,19 +12,20 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
-    POSTGRES_USER: str = Field(validation_alias="POSTGRES_USER")
-    POSTGRES_PASSWORD: str = Field(validation_alias="POSTGRES_PASSWORD")
-    POSTGRES_DB: str = Field(validation_alias="POSTGRES_DB")
-    POSTGRES_PORT: str = Field(validation_alias="POSTGRES_PORT")
-    POSTGRES_HOST: str = Field(validation_alias="POSTGRES_HOST")
-    debank_api_key: str = Field(validation_alias="DEBANK_API_KEY")
-    base_api_key: str = Field(validation_alias='BASE_API_KEY')
-    base_rpc: str = Field(validation_alias='BASE_HTTPS_URL')
-    base_websocket: str = Field(validation_alias='BASE_WEBSOCKET_URL')
-    base_scan_api_key: str = Field(validation_alias='BASE_SCAN_API_KEY')
-    account_pk: str = Field(validation_alias='ACCOUNT_1_PK')
-    jina_api_key : str = Field(validation_alias='JINA_API_KEY')
-    coin_gecko_api_key : str = Field(validation_alias='COIN_GECKO_API_KEY')
+    # POSTGRES_USER: str = Field(validation_alias="POSTGRES_USER")
+    # POSTGRES_PASSWORD: str = Field(validation_alias="POSTGRES_PASSWORD")
+    # POSTGRES_DB: str = Field(validation_alias="POSTGRES_DB")
+    # POSTGRES_PORT: str = Field(validation_alias="POSTGRES_PORT")
+    # POSTGRES_HOST: str = Field(validation_alias="POSTGRES_HOST")
+    # debank_api_key: str = Field(validation_alias="DEBANK_API_KEY")
+    # base_api_key: str = Field(validation_alias="BASE_API_KEY")
+    # base_rpc: str = Field(validation_alias='BASE_HTTPS_URL')
+    # base_websocket: str = Field(validation_alias='BASE_WEBSOCKET_URL')
+    base_scan_api_key: str = Field(validation_alias="BASE_SCAN_API_KEY")
+    # account_pk: str = Field(validation_alias='ACCOUNT_1_PK')
+    # jina_api_key : str = Field(validation_alias='JINA_API_KEY')
+    # coin_gecko_api_key: str = Field(validation_alias="COIN_GECKO_API_KEY")
+
 
 def get_postgres_uri(settings: Settings = Settings()) -> str:
     return f"postgresql://{settings.POSTGRES_USER}:{settings.POSTGRES_PASSWORD}@{settings.POSTGRES_HOST}:5432/{settings.POSTGRES_DB}"
