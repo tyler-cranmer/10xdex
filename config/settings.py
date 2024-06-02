70481525b5fcb55b36a815aaeb05a6a00871eeee
tyler-cranmer <tyler.cranmer@outlook.com>
2024-06-02 10:51:00 -0600

    stuff

--- config/settings.py
+++ config/settings.py
@@ -5,26 +5,31 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     """Settings config
 
-    Attributes:
-        debank_api_key (str): Debank API key
+    This class is used to define the settings for the application.
+
     """
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
     )
-    # POSTGRES_USER: str = Field(validation_alias="POSTGRES_USER")
-    # POSTGRES_PASSWORD: str = Field(validation_alias="POSTGRES_PASSWORD")
-    # POSTGRES_DB: str = Field(validation_alias="POSTGRES_DB")
-    # POSTGRES_PORT: str = Field(validation_alias="POSTGRES_PORT")
-    # POSTGRES_HOST: str = Field(validation_alias="POSTGRES_HOST")
-    # debank_api_key: str = Field(validation_alias="DEBANK_API_KEY")
-    # base_api_key: str = Field(validation_alias="BASE_API_KEY")
-    # base_rpc: str = Field(validation_alias='BASE_HTTPS_URL')
-    # base_websocket: str = Field(validation_alias='BASE_WEBSOCKET_URL')
+    # DATABASE KEYS
+    POSTGRES_USER: str = Field(validation_alias="POSTGRES_USER")
+    POSTGRES_PASSWORD: str = Field(validation_alias="POSTGRES_PASSWORD")
+    POSTGRES_DB: str = Field(validation_alias="POSTGRES_DB")
+    POSTGRES_PORT: str = Field(validation_alias="POSTGRES_PORT")
+    POSTGRES_HOST: str = Field(validation_alias="POSTGRES_HOST")
+
+    # BASE RPCs
+    cdp_rpc: str = Field(validation_alias="CDP_RPC")
+    ankr_rpc: str = Field(validation_alias="ANKR_RPC")
+    blast_rpc: str = Field(validation_alias="BLAST_RPC")
+    alchemy_rpc: str = Field(validation_alias="ALCHEMY_RPC")
+
+    # ANCILLARY KEYS
+    debank_api_key: str = Field(validation_alias="DEBANK_API_KEY")
+    coin_gecko_api_key: str = Field(validation_alias="COIN_GECKO_API_KEY")
     base_scan_api_key: str = Field(validation_alias="BASE_SCAN_API_KEY")
-    # account_pk: str = Field(validation_alias='ACCOUNT_1_PK')
-    # jina_api_key : str = Field(validation_alias='JINA_API_KEY')
-    # coin_gecko_api_key: str = Field(validation_alias="COIN_GECKO_API_KEY")
+    jina_api_key: str = Field(validation_alias="JINA_API_KEY")  # LLM SCRAPING API KEY
 
 
 def get_postgres_uri(settings: Settings = Settings()) -> str:
